47452d319eb0af09dc2773ae0094f6ef0c99bf50
Ekaterina Verbilova <cverbilova@gmail.com>
2025-12-16 02:13:10 +0300

    routing+modalki but smth went wrong with css

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,87 @@
-import { ConstructorPage } from '@pages';
-import '../../index.css';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+import { IngredientDetails, OrderInfo, Modal } from '@components';
+import './../../index.css';
 import styles from './app.module.css';
-
 import { AppHeader } from '@components';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { FC } from 'react';
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+const App: FC = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const background = location.state?.background;
+
+  const handleModalClose = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes location={background || location}>
+        {/* Основные маршруты */}
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        {/* Маршруты аутентификации */}
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        {/* Профиль */}
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        {/* Динамические маршруты - отдельные страницы */}
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='/feed/:number' element={<OrderInfo />} />
+        <Route path='/profile/orders/:number' element={<OrderInfo />} />
+        {/* 404 */}
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+
+      {/* Модальные окна для фоновых маршрутов */}
+      {background && (
+        <Routes>
+          {/* Модалка для деталей ингредиента */}
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='Детали ингредиента' onClose={handleModalClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          {/* Модалка для заказа из ленты */}
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='' onClose={handleModalClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          {/* Модалка для заказа из профиля */}
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='' onClose={handleModalClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </div>
+  );
+};
 
 export default App;
