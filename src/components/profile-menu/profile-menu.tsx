605885a4a935f9dff95d2538836d035891b5581f
Ekaterina Verbilova <cverbilova@gmail.com>
2026-01-04 17:46:41 +0300

    added logout to ProfileMenu

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,24 @@
 import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom'; // Добавить useNavigate
 import { ProfileMenuUI } from '@ui';
+import { useDispatch } from '../../services/store'; // Добавить
+import { logoutUser } from '../../services/slices/userSlice'; // Добавить
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate(); // Добавить
+  const dispatch = useDispatch(); // Добавить
 
-  const handleLogout = () => {};
+  const handleLogout = () => {
+    dispatch(logoutUser())
+      .unwrap()
+      .then(() => {
+        navigate('/login'); // Перенаправляем на страницу логина
+      })
+      .catch((error) => {
+        console.error('Logout error:', error);
+      });
+  };
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
