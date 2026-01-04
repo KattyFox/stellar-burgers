eef817a9a168442886668fc8a776911d602bbbd8
Ekaterina Verbilova <cverbilova@gmail.com>
2026-01-04 20:06:32 +0300

    fixed log out in ProfileMenu.tsx by using async

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,23 +1,21 @@
 import { FC } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom'; // Добавить useNavigate
+import { useLocation, useNavigate } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
-import { useDispatch } from '../../services/store'; // Добавить
-import { logoutUser } from '../../services/slices/userSlice'; // Добавить
+import { useDispatch } from '../../services/store';
+import { logoutUser } from '../../services/slices/userSlice';
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
-  const navigate = useNavigate(); // Добавить
-  const dispatch = useDispatch(); // Добавить
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const handleLogout = () => {
-    dispatch(logoutUser())
-      .unwrap()
-      .then(() => {
-        navigate('/login'); // Перенаправляем на страницу логина
-      })
-      .catch((error) => {
-        console.error('Logout error:', error);
-      });
+  const handleLogout = async () => {
+    try {
+      await dispatch(logoutUser()).unwrap();
+      navigate('/login');
+    } catch (error) {
+      console.error('Ошибка при выходе:', error);
+    }
   };
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
